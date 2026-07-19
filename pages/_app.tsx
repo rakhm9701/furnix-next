27c4f38898b8c6143c801ae917385e35f41d40ed
Rakhmidin Odilov <odilovrakhmidin@gmail.com>
2026-07-20 02:37:41 +0900

    fix: fixed sccs import path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 import { light } from '../scss/MaterialTheme';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../apollo/client';
 import { appWithTranslation } from 'next-i18next';
+import nextI18NextConfig from '../next-i18next.config';
 import '../scss/app.scss';
 import '../scss/pc/main.scss';
 import '../scss/mobile/main.scss';
 import { CartProvider } from '../libs/context/useCart';
 
 const App = ({ Component, pageProps }: AppProps) => {
-	// @ts-ignore
-	const [theme, setTheme] = useState(createTheme(light));
+	const theme = useMemo(() => createTheme(light as any), []);
 	const client = useApollo(pageProps.initialApolloState);
 
 	return (
@@ -28,4 +28,4 @@ const App = ({ Component, pageProps }: AppProps) => {
 	);
 };
 
-export default appWithTranslation(App);
+export default appWithTranslation(App, nextI18NextConfig as any);
